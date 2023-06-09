1249d1549b553dd80f1b17a6965e47ef6e0ef3bc
Konstantin Keller <koskeller@gmail.com>
2023-06-09 11:22:42 +0300

    feat: insertion sort

---
+++ src/insertion_sort.rs
@@ -0,0 +1,50 @@
+trait InsertionSort<T> {
+    fn insertion_sort(&mut self)
+    where
+        T: Ord;
+}
+
+impl<T> InsertionSort<T> for Vec<T>
+where
+    T: Ord,
+{
+    // Worst case time complexity: Θ(n^2)
+    // Average case time complexity: Θ(n^2)
+    // Best case time complexity: Θ(n)
+    // Space complexity: Θ(1)
+    fn insertion_sort(&mut self) {
+        if self.len() <= 1 {
+            return;
+        }
+
+        for i in 1..self.len() {
+            for j in (1..=i).rev() {
+                if self[j] < self[j - 1] {
+                    self.swap(j, j - 1);
+                }
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let mut tests = vec![
+            (vec![], vec![]),
+            (vec![1], vec![1]),
+            (vec![1, 2], vec![1, 2]),
+            (vec![2, 1], vec![1, 2]),
+            (vec![3, 1, -0, 5, -1, 44, 0], vec![-1, -0, 0, 1, 3, 5, 44]),
+            (vec![3, 0, 0, 5, 0, 0, 1], vec![0, 0, 0, 0, 1, 3, 5]),
+        ];
+
+        for (got, want) in tests.iter_mut() {
+            got.insertion_sort();
+            assert_eq!(got, want);
+        }
+    }
+}
